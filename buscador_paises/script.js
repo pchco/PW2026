a71cf6f81b299f600503cf1e4e652e622101d73f
pchco <vitoriapachecodossantos18@gmail.com>
2026-03-10 19:10:25 -0300

    commits de atualização do buscador de países

--- buscador_paises/script.js
+++ buscador_paises/script.js
@@ -1,8 +1,9 @@
+var allCountries = [];
+
 async function searchCountry(param) {
   let url = "https://restcountries.com/v3.1/" + param;
   // let url = "https://restcountries.com/v3.1/all?fields=name,flags,translations"
   // let url = "https://restcountries.com/v3.1/region/oceania"
-  let allCountries = [];
 
   // Comunicação com API
   let response = await fetch(url);
@@ -35,6 +36,36 @@ function searchCountryByRegion(input){
     url = "region/" + input.value
 
   searchCountry(url)
+}
+
+//Buscando o país digitado pelo usuário
+function searchCountryByUser(input){
+  let countrySearched = input.value.toLowerCase()
+  let countriesFound = []
+
+  console.log(countrySearched)
+
+  //Procurando o país de acordo com as letras digitadas
+  for(let pais of allCountries){
+    let name = pais.translations.por.common.toLowerCase()
+    if(name.startsWith(countrySearched)){
+      countriesFound.push(pais)
+    }
+  }
+  document.querySelector(".allCountries").innerHTML = ""
+
+  // Criando card do País
+  for (let country of countriesFound) {
+    let cardCountry = document.createElement("div");
+    cardCountry.classList.add("country");
+    cardCountry.innerHTML = `
+        <img
+          width="200px"
+          src="${country.flags.png}"
+        />
+        <p>${country.name.common}</p>`;
+    document.querySelector(".allCountries").append(cardCountry);
+  }
 }
 
 searchCountry("all?fields=name,flags,translations");
